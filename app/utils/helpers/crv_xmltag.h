75e7d28317998f563450eaf8bef20bdbe52decae
tamszod <tamszod@gmail.com>
2026-04-06 00:47:48 +0200

    WIP

---
+++ app/utils/helpers/crv_xmltag.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+namespace crv {
+    namespace xml {
+       namespace tag {
+            static const constexpr char* GRAPHICAL_OBJECTS = "graphical_objects";
+        }
+    }
+}
